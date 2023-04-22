2c8aea11f57f77ddb7406aa8d98b409aeef02153
Luan Sá <101113009+LVSA2004@users.noreply.github.com>
2023-04-21 21:30:16 -0300

    Delete global directory

--- global/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module br.com.fiap {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens br.com.fiap.controller to javafx.fxml;
-    opens br.com.fiap.model to javafx.base;
-    exports br.com.fiap;
-}
